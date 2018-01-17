887ce6c6928cf0144e65210b8f0316f302f5b7c5
Hector Lovo <lovohh@gmail.com>
2018-01-16 20:48:06 -0500

    Adding Temp 'main' Code & 'godep' Dependencies
    Specifically:
    - Adding tests dependencies, or dependencies needed
      to run unit-tests

---
+++ infrastructure/deps/tests.go
@@ -0,0 +1,5 @@
+package deps
+
+import (
+	_ "github.com/stretchr/testify/assert"
+)
--- main.go
+++ main.go
@@ -1,4 +1,25 @@
 package main
 
+import (
+	"fmt"
+	gmux "github.com/gorilla/mux"
+	_ "github.com/lovohh/lovohhwebapi/infrastructure/deps"
+	"net/http"
+)
+
 func main() {
+	router := gmux.NewRouter()
+
+	router.HandleFunc("/test", func(rw http.ResponseWriter, req *http.Request) {
+		rw.Write([]byte("Hello world"))
+	})
+
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: router,
+	}
+
+	if serverErr := server.ListenAndServe(); serverErr != nil {
+		fmt.Println("Server failed to boot: " + serverErr.Error())
+	}
 }
